3a76a23064b1dc0cb0e01f651c470f4d694a0ba1
praveen08052010 <97897309+praveen08052010@users.noreply.github.com>
2022-06-06 19:38:09 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB5dh7RabMzQni2hkaitvp5hO4HV-YYSME",
+    authDomain: "let-schat-284b4.firebaseapp.com",
+    databaseURL: "https://let-schat-284b4-default-rtdb.firebaseio.com",
+    projectId: "let-schat-284b4",
+    storageBucket: "let-schat-284b4.appspot.com",
+    messagingSenderId: "519669827121",
+    appId: "1:519669827121:web:de20d93e1f99b2c464b710"
+  };
+  
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
